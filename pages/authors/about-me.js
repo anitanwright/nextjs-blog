03680a6f52c17eb0afda84f90b309c2858af181d
Anita Wright <anitanwright@gmail.com>
2021-11-22 12:39:13 -0700

    added images and blog pages

--- pages/authors/about-me.js
+++ pages/authors/about-me.js
@@ -1,16 +1,16 @@
 import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../../components/layout'
 
 export default function AboutMe() {
   return (
-    <>
-      <h1>This page is all about me</h1>
-    <p>Welcome to my page</p>
+    <Layout>
+      <p>Welcome to my page!</p>
       <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
+     
       </h2>
-    </>
+      </Layout>
+  
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -3,32 +3,33 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 
-
 export default function Home() {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
+     
       <section className={utilStyles.headingMd}>
         <p>Hello!  My name is Anita.  I'm a software developer and kitten lover!</p>
-      
-        {' '}
+        <p>
         <Link href="/posts/first-post">
         <a>Here is my first post!</a>
         </Link>
-        {' '}
+        </p>
+        <p>
         <Link href="/posts/second-post">
         <a>Here is my second post!</a>
         </Link>
-        {' '}
+        </p>
+        <p>
         <Link href="/authors/about-me">
         <a>Here you can read about me!</a>
         </Link>
+        </p>
 
           (This is a sample website - you will be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        
       </section>
     </Layout>
   )
